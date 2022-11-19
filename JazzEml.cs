5e5ec6958cf461ff038bc7f6550b96a022477af3
Gunnar Liden <81466069+GunnarAarau@users.noreply.github.com>
2022-11-19 18:37:10 +0100

    Add files via upload

---
+++ JazzEml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eml
+{
+    /// <summary>Holds data for one email of the format EML
+    /// <para></para>
+    /// <para></para>
+    /// </summary>
+    public class JazzEml
+    {
+        /// <summary>Full name for the eml file</summary>
+        private string m_path_file_name_local = @"";
+        /// <summary>Get and set full name for the eml file</summary>
+        public string EmlFileNameLocal { get { return m_path_file_name_local; } set { m_path_file_name_local = value; } }
+
+        /// <summary>Server path for the eml file</summary>
+        private string m_path_server = @"";
+        /// <summary>Get and set server path for the eml file</summary>
+        public string EmlPathServer { get { return m_path_server; } set { m_path_server = value; } }
+
+        /// <summary>Server name for the eml file</summary>
+        private string m_file_server = @"";
+        /// <summary>Get and set server name for the eml file</summary>
+        public string EmlFileServer { get { return m_file_server; } set { m_file_server = value; } }
+
+        /// <summary>Send year</summary>
+        private int m_send_year = -12345;
+        /// <summary>Get and set send year</summary>
+        public int SendYear { get { return m_send_year; } set { m_send_year = value; } }
+
+        /// <summary>Send month</summary>
+        private int m_send_month = -12345;
+        /// <summary>Get and set send month</summary>
+        public int SendMonth { get { return m_send_month; } set { m_send_month = value; } }
+
+        /// <summary>Send day</summary>
+        private int m_send_day = -12345;
+        /// <summary>Get and set send day</summary>
+        public int SendDay { get { return m_send_day; } set { m_send_day = value; } }
+
+        /// <summary>Email subject</summary>
+        private string m_subject = @"";
+        /// <summary>Get and set email subject</summary>
+        public string Subject { get { return m_subject; } set { m_subject = value; } }
+
+        /// <summary>Email from</summary>
+        private string m_from = @"";
+        /// <summary>Get and set email from</summary>
+        public string From { get { return m_from; } set { m_from = value; } }
+
+        /// <summary>Email to</summary>
+        private string m_to = @"";
+        /// <summary>Get and set email to</summary>
+        public string To { get { return m_to; } set { m_to = value; } }
+
+        /// <summary>Message text</summary>
+        private string m_message_text = @"";
+        /// <summary>Get and set message text</summary>
+        public string MsgText { get { return m_message_text; } set { m_message_text = value; } }
+
+        /// <summary>Message HTML</summary>
+        private string m_message_html = @"";
+        /// <summary>Get and set message HTML</summary>
+        public string MsgHtml { get { return m_message_html; } set { m_message_html = value; } }
+
+        /// <summary>Local attachment image name</summary>
+        private string m_attachment_image_name_local = @"";
+        /// <summary>Get and set the local attachment image name</summary>
+        public string AttachmentImageLocal { get { return m_attachment_image_name_local; } set { m_attachment_image_name_local = value; } }
+
+        /// <summary>Server attachments path</summary>
+        private string m_attachment_image_path_server = @"";
+        /// <summary>Get and set the server attachments path</summary>
+        public string AttachmentImagePathServer { get { return m_attachment_image_path_server; } set { m_attachment_image_path_server = value; } }
+
+        /// <summary>Server attachment image file name</summary>
+        private string m_attachment_image_server = @"";
+        /// <summary>Get and set the server attachment image file name</summary>
+        public string AttachmentImageServer { get { return m_attachment_image_server; } set { m_attachment_image_server = value; } }
+
+        /// <summary>Embedded poster flag</summary>
+        private bool m_b_embedded_poster = false;
+        /// <summary>Get and set embedded poster flag</summary>
+        public bool EmbeddedPosterFlag { get { return m_b_embedded_poster; } set { m_b_embedded_poster = value; } }
+
+        /// <summary>Local attachment file name</summary>
+        private string m_attachment_file_name_local = @"";
+        /// <summary>Get and set the local attachment file name</summary>
+        public string AttachmentFileLocal { get { return m_attachment_file_name_local; } set { m_attachment_file_name_local = value; } }
+
+        /// <summary>Server attachments path</summary>
+        private string m_attachment_path_server = @"";
+        /// <summary>Get and set the server attachments path</summary>
+        public string AttachmentPathServer { get { return m_attachment_path_server; } set { m_attachment_path_server = value; } }
+
+        /// <summary>Server attachment file name</summary>
+        private string m_attachment_file_server = @"";
+        /// <summary>Get and set the server attachment file name</summary>
+        public string AttachmentFileServer { get { return m_attachment_file_server; } set { m_attachment_file_server = value; } }
+
+        /// <summary>Returns the img html element string for the embedded poster of the newsletter</summary>
+        public string GetImgHtmlString()
+        {
+            string ret_img_html = @"";
+
+            if (m_attachment_image_server.Length < 4)
+            {
+                // Image server file name is not set.
+                return ret_img_html;
+            }
+
+            if (m_attachment_image_path_server.Length == 0)
+            {
+                // Image server path is not set.
+                return ret_img_html;
+            }
+
+            ret_img_html = @"<img src= '" + m_attachment_image_path_server + m_attachment_image_server + 
+                           @"' width= '259' 'alt= 'Plakat' class= 'cl_newsletter_embedded_img'>";
+
+            return ret_img_html;
+
+        } // GetImgHtmlString
+
+
+    } // JazzEml
+
+} // namespace
---
+++ SaveEml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.IO;
+using System.Reflection;
+using System.Net.Mime;
+
+namespace Eml
+{
+    /// <summary>Save of an email in the EML format
+    /// <para></para>
+    /// <para></para>
+    /// </summary>
+    public static class SaveEml
+    {
+        #region Internet links to EML information
+
+        // https://www.codeguru.com/csharp/csharp/cs_internet/mail/saving-an-e-mail-as-an-.eml-file-in-c.html
+
+        #endregion // Internet links to EML information
+
+        #region Main execution function
+
+        /// <summary>Save of an email in the EML format
+        /// <para></para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="ref_eml">JazzEml object with all data for the email</param>
+        /// <param name="o_error">Error message</param>
+        public static bool Execute(ref JazzEml ref_eml, out string o_error)
+        {
+            o_error = @"";
+
+            string to_str = ref_eml.To;
+
+            string from_str = ref_eml.From;
+
+            string subject_str = ref_eml.Subject;
+
+            string body_str = @"<body>" + ref_eml.MsgHtml + @"</body>";
+
+            string email_dir_str = Path.GetDirectoryName(ref_eml.EmlFileNameLocal);
+
+            string msg_name_str = Path.GetFileName(ref_eml.EmlFileNameLocal);
+
+            using (var smtp_client = new SmtpClient())
+            {
+                MailMessage mail_msg = new MailMessage(from_str, to_str, subject_str, body_str);
+
+                AlternateView html_view = AlternateView.CreateAlternateViewFromString(body_str, null, "text/html");
+
+                mail_msg.AlternateViews.Add(html_view);
+
+                AddAttachmentToMail(ref_eml.AttachmentImageLocal, ref mail_msg);
+
+                AddAttachmentToMail(ref_eml.AttachmentFileLocal, ref mail_msg);
+
+                smtp_client.UseDefaultCredentials = true;
+
+                smtp_client.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+
+                smtp_client.PickupDirectoryLocation = email_dir_str;
+
+                try
+                {
+                    smtp_client.Send(mail_msg);
+                }
+                catch (Exception ex)
+                {
+                    o_error = @"SaveEml.Execute SmtpClient.Send failed " + ex.ToString();
+
+                    return false;
+                }
+
+            } // using SmtpClient
+
+            var default_msg_path = new DirectoryInfo(email_dir_str).GetFiles().OrderByDescending(f => f.LastWriteTime).First();
+
+            string warning_msg = @"";
+
+            var real_msg_path = Path.Combine(email_dir_str, msg_name_str);
+            try
+            {
+                File.Move(default_msg_path.FullName, real_msg_path);
+               
+            }
+            catch (System.IO.IOException e)
+            {
+                warning_msg = e.ToString();
+
+                File.Delete(real_msg_path);
+
+                File.Move(default_msg_path.FullName, real_msg_path);
+            }
+
+            return true;
+
+        } // Execute
+
+        #endregion // Main execution function
+
+        /// <summary>Add attachement</summary>
+        private static void AddAttachmentToMail(string i_attachment_file_name, ref MailMessage o_mail_poster)
+        {
+            if (i_attachment_file_name == "")
+            {
+                return;
+            }
+
+            if (!File.Exists(i_attachment_file_name))
+            {
+                return;
+            }
+
+            Attachment data = new Attachment(i_attachment_file_name, MediaTypeNames.Application.Octet);
+            // Add time stamp information for the file.
+            ContentDisposition disposition = data.ContentDisposition;
+            disposition.CreationDate = System.IO.File.GetCreationTime(i_attachment_file_name);
+            disposition.ModificationDate = System.IO.File.GetLastWriteTime(i_attachment_file_name);
+            disposition.ReadDate = System.IO.File.GetLastAccessTime(i_attachment_file_name);
+            // Add the file attachment to this e-mail message.
+            o_mail_poster.Attachments.Add(data);
+
+        } // AddAttachmentToMail
+
+    } // SaveEml
+
+} // namespace
